d109110ac6bbbae523dcabe3712743192e64b94d
abkardev <ammg.11@hotmail.com>
2022-07-01 19:49:44 +0300

    add card icon and cart dropdown components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@
  import {Routes, Route} from 'react-router-dom'
  import Navigation from './router/navigation/navigation.component'
  import Authentication from './router/authentication/authentication.component'
+ import Shop from './router/shop/shop.component'
+ 
 
- const Shop = () => {
-   return <h1>I am the Shop Page</h1>
- }
 const App = () => {
   
   return (
--- src/router/navigation/navigation.component.jsx
+++ src/router/navigation/navigation.component.jsx
@@ -1,13 +1,16 @@
 import { Fragment , useContext} from "react";
 import {Outlet, Link} from 'react-router-dom';
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component'
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg'
 import { userContext } from "../../contexts/user.context";
+import {CartContext} from '../../contexts/cart.context'
 import {signOutUser} from '../../Utils/firebase/firebase-utils'
 import './navigation.styles.scss'
 
 const Navigation = () => {
   const{currentUser} = useContext(userContext);
- 
+  const {isCartOpen} = useContext(CartContext);
     return(
         <Fragment>
             <div className="navigation">
@@ -23,8 +26,9 @@ const Navigation = () => {
                         ): (<Link className="nav-link" to={'/auth'}>
                         Sign In
                     </Link>)}
-                    
+                    <CartIcon/>
                 </div>
+                {isCartOpen && <CartDropdown/>}
             </div>
             <Outlet />
         </Fragment>
